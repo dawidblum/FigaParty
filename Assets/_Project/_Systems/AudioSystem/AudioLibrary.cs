c7bd71126cf4c3cd1c224acaba3c4efdc9d2d40a
CDV\dblumczy <dawidblu@protonmail.com>
2024-04-06 15:29:09 +0200

    AudioLibrary split

---
+++ Assets/_Project/_Systems/AudioSystem/AudioLibrary.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class AudioLibrary : MonoBehaviour {
+    [Serializable]
+    public struct SFXPair {
+        public AudioClip clip;
+        public SoundType type;
+    }
+    
+    [Serializable]
+    public struct MusicPair {
+        public AudioClip clip;
+        public MusicType type;
+    }
+
+    [SerializeField] private List<SFXPair> sfxList = new List<SFXPair>();
+    [SerializeField] private List<MusicPair> musicList = new List<MusicPair>();
+
+    public enum SoundType {
+        UI_Confirm,
+        UI_Cancel,
+        UI_Deny,
+        UI_Hover,
+        Time_Low,
+        Minigame_Intro,
+        Minigame_Fail,
+        Minigame_Win
+    } 
+    
+    public enum MusicType {
+        Menu_Theme,
+        Find_Figa_Theme,
+        Dig_To_Fig_Theme,
+        Figa_Makes_Pizza_Theme,
+        Figa_Jam_Theme,
+        Figaro_Theme
+    }
+
+    public AudioClip GetAudioClip(SoundType _type) {
+        return sfxList.Where(_sfxPair => _sfxPair.type == _type)
+            .Select(_sfxPair => _sfxPair.clip).FirstOrDefault();
+    }
+    public AudioClip GetAudioClip(MusicType _type) {
+        return musicList.Where(_musicPair => _musicPair.type == _type)
+            .Select(_musicPair => _musicPair.clip).FirstOrDefault();
+    }
+    
+    
+    
+    
+}
